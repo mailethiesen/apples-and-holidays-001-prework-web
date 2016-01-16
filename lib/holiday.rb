a2cb31e6545eb0eabc10290fcefa632605e03799
mailethiesen <mailethiesen@gmail.com>
2016-01-15 22:02:52 -0500

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,32 +18,44 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above 
   # add the second argument, which is a supply, to BOTH the 
   # Christmas AND the New Year's arrays
-  
+  holiday_hash[:winter].each do |season, holiday|
+    holiday_hash[:winter][season] = supply
+  end
 end
  
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-  
+  holiday_hash[:spring][:memorial_day] = supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash[season][holiday_name] = supply_array
   # remember to return the updated hash
+  holiday_hash
   
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  
+  new_array = holiday_hash[:winter].collect do |holiday, supplies|
+    supplies
+    # binding.pry
+  end
+  new_array.flatten
+
 end
+
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -53,12 +65,28 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+    holiday_hash.each do |season, holiday|
+        puts "#{season.capitalize}:"
+        holiday.each do |k, v|
+            puts "  #{k.to_s.split(/ |\_/).map(&:capitalize).join(" ")}: #{v.join(", ")}"
+        end
+    end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+ new_array = holiday_hash.collect do |season, holiday|
+     holiday.collect do |k,v|
+         if v.include?("BBQ")
+            k
+         end
+     end
+  end
+new_array.select do |i|
+    i.delete_if {|j| j == nil}
+end
+new_array.flatten
 
 end
 
